375da468b252d9a146e95e9c3b77a264026e345d
LizRuelas <liz.ruelas.borda@gmail.com>
2016-12-06 15:38:21 -0500

    agregando los votos

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ var store = makeStore();
 
 startServer(store);
 
-
-
 store.dispatch({
   	type: 'SET_ENTRIES',
   	entries: require('./entries.json')
+});
+
+store.dispatch({
+  	type: 'NEXT',
 });
